ec73eab58f3f5f32f551511021cd2ef63b20bd26
acrow18 <acrow18@usd112.org>
2023-03-05 20:08:30 -0600

    Added Opening and Closing Applications to project. output.txt file has content when using voice, is possibly not being read correctly. Not sure the error

--- Keyboard.py
+++ Keyboard.py
@@ -1,98 +1,169 @@
 from time import sleep
+from AppOpener import open, close
 import pyautogui
 import os
 from sounddevice import wait
 import pydirectinput
 import random
 import pyowm
+import pyttsx3
 
+def Movement(option):
 #Used to get rid of any sort of punctuation
-def tokenizingInput(option):
-    
-    if "." in option:
-        optionsplit = option.split(".", 1)
-        
-        if(optionsplit[0] == optionsplit[1]):
-            option = optionsplit[0]
-        else:
-            option = option.replace(".", "")
-    option = option.replace("?", "")
-    option = option.replace("!", "")
-    option = option.replace(",", "")
-    
-    print(option)
-    
-    option = option.lower()
-    
-    print(option)
-    
-    _split = option.split(" ", 1)
-    _completeSplit = option.split(" ")
+    def tokenizingInput(option):
 
-#Used to open lnk files through the command prompt
-def openFilesThroughCommandPrompt(option):
-    _split = tokenizingInput(option)
-    
-    if(_split[0] == "open," or _split[0] == "open"):
-        os.system('cmd /c "cd lnks && START {name}.lnk"'.format(name = _split[1])) #types in the command prompt START and the name of the file to open
+        if "." in option:
+            optionsplit = option.split(".", 1)
 
-#Used to type words
-def typingWords(option):
-    _split = tokenizingInput(option)
+            if(optionsplit[0] == optionsplit[1]):
+                option = optionsplit[0]
+            else:
+                option = option.replace(".", "")
+        option = option.replace("?", "")
+        option = option.replace("!", "")
+        option = option.replace(",", "")
 
-    if(_split[0] == "type"):
-        pydirectinput.write(_split[1])
+        print(option)
 
-#Common key presses
-def commonFunctionKeys(option):
-    _split = tokenizingInput(option)
-    
-    if(_split[0] == "press" or _split[0] == "press,"):
-        if(_split[1] == "enter"):
-            pydirectinput.press("enter")
-        elif(_split[1] == "backspace"):
-            pydirectinput.press("backspace")
-        elif(_split[1] == "tab"):
-            pydirectinput.press("tab")
-        elif(_split[1] == "escape"):
-            pydirectinput.press("esc")
-        elif(_split[1] == "space"):
-            pydirectinput.press("space")
-        elif(_split[1] == "right"):
-            pydirectinput.press("right")
-        elif(_split[1] == "left"):
-            pydirectinput.press("left")
-        elif(_split[1] == "up"):
-            pydirectinput.press("up")
-        elif(_split[1] == "down"):
-            pydirectinput.press("down")
+        option = option.lower()
 
-#Used to captialize or lowercase letters          
-def lowerOrUpper(option):
-    _split = tokenizingInput(option)
-         
-    if(_split[0] == "lower" or _split[0] == "lower,"):
-        pydirectinput.press(_split[1][0])     
+        print(option)
 
-    if(_split[0] == "upper" or _split[0] == "upper,"):
-        with pyautogui.hold("shift"):
-            pydirectinput.press(_split[1][0])    
-            
-def openCommon(option):
-    _split = tokenizingInput(option)
-    
-    #opens google chrome then displays youtube
-    if(option.startswith("use youtube")):
-        Movement("open google")
-        
-        sleep(6)
+        _split = option.split(" ", 1)
+        _completeSplit = option.split(" ")
 
-        pyautogui.write("youtube.com")
-        pyautogui.press("enter")
+    #Used to open lnk files through the command prompt
+    #def openFilesThroughCommandPrompt(option):
+     #_split = tokenizingInput(option)
+       # if(_split[0] == "open," or _split[0] == "open"):
+        #    os.system('cmd /c "cd lnks && START {name}.lnk"'.format(name = _split[1])) #types in the command prompt START and the name of the file to open
 
-        sleep(6)
 
-        for i in range(4):
-            pydirectinput.press("tab")
-        Movement("type " + option.replace("use youtube ", ""))
-        Movement("press enter")
+    #Used to open applications through CLI
+    def openAppsThroughCLI(option):
+        _split = tokenizingInput(option)
+        if (_split[0] == "Open" or _split[0] == "open"):
+            open(option, match_closest=True)
+
+    #Used to close applications through CLI
+    def closeAppsThroughCLI(option):
+        _split = tokenizingInput(option)
+        if (_split[0] == "Close" or _split[0] == "close"):
+            close(option, match_closest=True, output=False)
+
+    #Used to type words
+    def typingWords(option):
+        _split = tokenizingInput(option)
+
+        if(_split[0] == "type"):
+            pydirectinput.write(_split[1])
+
+    #Common key presses
+    def commonFunctionKeys(option):
+        _split = tokenizingInput(option)
+
+        if(_split[0] == "press" or _split[0] == "press,"):
+            if(_split[1] == "enter"):
+                pydirectinput.press("enter")
+            elif(_split[1] == "backspace"):
+                pydirectinput.press("backspace")
+            elif(_split[1] == "tab"):
+                pydirectinput.press("tab")
+            elif(_split[1] == "escape"):
+                pydirectinput.press("esc")
+            elif(_split[1] == "space"):
+                pydirectinput.press("space")
+            elif(_split[1] == "right"):
+                pydirectinput.press("right")
+            elif(_split[1] == "left"):
+                pydirectinput.press("left")
+            elif(_split[1] == "up"):
+                pydirectinput.press("up")
+            elif(_split[1] == "down"):
+                pydirectinput.press("down")
+
+    #Used for all single letter key presses
+    def singleLetterKeyPresses(option):
+        _split = tokenizingInput(option)
+
+        if(_split[0] == "press" or _split[0] == "press,"):
+            if(_split[1] == "a"):
+                pydirectinput.press("a")
+            elif(_split[1] == "b"):
+                pydirectinput.press("b")
+            elif(_split[1] == "c"):
+                pydirectinput.press("c")
+            elif(_split[1] == "d"):
+                pydirectinput.press("d")
+            elif(_split[1] == "e"):
+                pydirectinput.press("e")
+            elif(_split[1] == "f"):
+                pydirectinput.press("f")
+            elif(_split[1] == "g"):
+                pydirectinput.press("g")
+            elif(_split[1] == "h"):
+                pydirectinput.press("h")
+            elif(_split[1] == "i"):
+                pydirectinput.press("i")
+            elif(_split[1] == "j"):
+                pydirectinput.press("j")
+            elif(_split[1] == "l"):
+                pydirectinput.press("l")
+            elif(_split[1] == "m"):
+                pydirectinput.press("m")
+            elif(_split[1] == "n"):
+                pydirectinput.press("n")
+            elif(_split[1] == "o"):
+                pydirectinput.press("o")
+            elif(_split[1] == "p"):
+                pydirectinput.press("p")
+            elif(_split[1] == "q"):
+                pydirectinput.press("q")
+            elif(_split[1] == "r"):
+                pydirectinput.press("r")
+            elif(_split[1] == "s"):
+                pydirectinput.press("s")
+            elif(_split[1] == "t"):
+                pydirectinput.press("t")
+            elif(_split[1] == "u"):
+                pydirectinput.press("u")
+            elif(_split[1] == "v"):
+                pydirectinput.press("v")
+            elif(_split[1] == "w"):
+                pydirectinput.press("w")
+            elif(_split[1] == "x"):
+                pydirectinput.press("x")
+            elif(_split[1] == "y"):
+                pydirectinput.press("y")
+            elif(_split[1] == "z"):
+                pydirectinput.press("z")
+
+    #Used to captialize or lowercase letters
+    def lowerOrUpper(option):
+        _split = tokenizingInput(option)
+
+        if(_split[0] == "lower" or _split[0] == "lower,"):
+            pydirectinput.press(_split[1][0])
+
+        if(_split[0] == "upper" or _split[0] == "upper,"):
+            with pyautogui.hold("shift"):
+                pydirectinput.press(_split[1][0])
+
+    def openCommon(option):
+        _split = tokenizingInput(option)
+
+        #opens google chrome then displays youtube
+        if(option.startswith("use youtube")):
+            Movement("open google")
+
+            sleep(6)
+
+            pyautogui.write("youtube.com")
+            pyautogui.press("enter")
+
+            sleep(6)
+
+            for i in range(4):
+                pydirectinput.press("tab")
+            Movement("type " + option.replace("use youtube ", ""))
+            Movement("press enter")
