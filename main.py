e0d1688bc605beb93c9dd187a9fb93e5b0effe92
acrow18 <acrow18@usd112.org>
2023-03-20 13:47:58 -0500

    Added classes to VoiceIntake.py

--- main.py
+++ main.py
@@ -2,20 +2,21 @@ import VoiceIntake
 import Keyboard
 from sounddevice import wait
 
-#Main function
+
+# Main function
 def main():
-    while 1==1:
+    while 1 == 1:
         print("Recording")
-        VoiceIntake.record_voice()
+        VoiceIntake.VoiceInput.record_voice()
         text = VoiceIntake.request_file()
         with open("output.txt") as f:
             line = f.readline()
 
-            #process the voice recording
+            # process the voice recording
             Keyboard.CleaningUpInput.cleaningUpOption(line)
         wait(2)
 
-                                            
-#Used to implement main
-if(__name__ == "__main__"):
+
+# Used to implement main
+if __name__ == "__main__":
     main()
